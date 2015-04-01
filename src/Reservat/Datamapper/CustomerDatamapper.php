9d5e40bfaa3f8202ba0d0e70d5da950b0e6c22d1
Paul Westerdale <paul@westerdale.me>
2015-04-01 14:30:42 +0100

    Abstract out PDODatamapper logic

--- src/Reservat/Datamapper/CustomerDatamapper.php
+++ src/Reservat/Datamapper/CustomerDatamapper.php
@@ -2,26 +2,12 @@
 
 namespace Reservat\Datamapper;
 
-use Reservat\Core\Interfaces\DatamapperInterface;
+use Reservat\Core\Interfaces\SQLDatamapperInterface;
 use Reservat\Core\Interfaces\EntityInterface;
+use Reservat\Core\Datamapper\PDODatamapper;
 
-class CustomerDatamapper implements DatamapperInterface
+class CustomerDatamapper extends PDODatamapper implements SQLDatamapperInterface
 {
-    /**
-     * @var \PDO
-     */
-    protected $db = null;
-
-    /**
-     * Pass an instance of PDO through
-     *
-     * @param \PDO $pdo
-     * @throws \PDOException
-     */
-    public function __construct(\PDO $pdo)
-    {
-        $this->db = $pdo;
-    }
 
     /**
      * Return the table name we're interacting with
@@ -31,93 +17,5 @@ class CustomerDatamapper implements DatamapperInterface
     public function table()
     {
         return 'customer';
-    }
-
-    /**
-     * Attempt to insert a record into the Customer Storage engine
-     *
-     * @param EntityInterface $entity
-     */
-    public function insert(EntityInterface $entity)
-    {
-        $params = trim(str_repeat('?,', count($entity->toArray())), ',');
-        $query  = "INSERT INTO " . $this->table() . " (". implode(', ', array_keys($entity->toArray())).") ";
-        $query .= "VALUES (".$params.")";
-
-        $this->execute($query, array_values($entity->toArray()));
-    }
-
-    /**
-     * Update the customer entity
-     *
-     * @param EntityInterface $entity
-     * @param $id
-     */
-    public function update(EntityInterface $entity, $id)
-    {
-        $update = function() use($entity) {
-            $sql = 'UPDATE ' . $this->table() . ' SET ';
-            $sep = null;
-
-            foreach($entity->toArray() as $key => $value) {
-                $sql .= $sep . $key . ' = ' . '?';
-                $sep = ', ';
-            }
-
-            $sql .= ' WHERE id = ?';
-
-            return rtrim(trim($sql), ',');
-        };
-
-        $values = $entity->toArray() + array('id' => $id);
-
-        $this->execute($update(), array_values($values));
-    }
-
-    /**
-     * Attempt to update the Customer entity if the $id is passed, otherwise insert.
-     *
-     * @param EntityInterface $entity
-     * @param null $id
-     */
-    public function save(EntityInterface $entity, $id = null)
-    {
-        if(!is_null($id)) {
-            $this->update($entity, $id);
-        } else {
-            $this->insert($entity);
-        }
-    }
-
-    /**
-     * Attempt to delete the Customer entity
-     *
-     * @param EntityInterface $entity
-     * @param $id
-     */
-    public function delete(EntityInterface $entity, $id)
-    {
-        $query = 'DELETE FROM ' . $this->table() . ' WHERE id = ?';
-        $this->execute($query, array(intval($id)));
-    }
-
-    /**
-     * Execute the query we build
-     *
-     * @param $query
-     * @param $values
-     * @return bool
-     */
-    private function execute($query, array $values)
-    {
-        $this->db->beginTransaction();
-
-        if($this->db->prepare($query)->execute($values)) {
-            $this->db->commit();
-            return true;
-        }
-
-        $this->db->rollBack();
-        return false;
     }
 }
