1905c990b5e5dbcfcd7c089254b188743fc76410
Luke Steadman <luke@steadweb.co.uk>
2015-04-04 21:56:14 +0100

    [BUG] implement iterator interface

--- src/Reservat/Repository/CustomerRepository.php
+++ src/Reservat/Repository/CustomerRepository.php
@@ -4,7 +4,7 @@ namespace Reservat\Repository;
 
 use Reservat\Interfaces\RepositoryInterface;
 
-class CustomerRepository implements RepositoryInterface
+class CustomerRepository implements RepositoryInterface, \Iterator
 {
     /**
      * @var null|\PDO
@@ -17,12 +17,20 @@ class CustomerRepository implements RepositoryInterface
     protected $records = array();
 
     /**
+     * Use for \Iterator
+     *
+     * @var int
+     */
+    private $position = 0;
+
+    /**
      * Inject an instance of PDO
      *
      * @param \PDO $db
      */
     public function __construct(\PDO $db)
     {
+        $this->position = 0;
         $this->db = $db;
     }
 
@@ -108,4 +116,62 @@ class CustomerRepository implements RepositoryInterface
     {
         return 'customer';
     }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the current element
+     * @link http://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     */
+    public function current()
+    {
+        return $this->records[$this->position];
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Move forward to next element
+     * @link http://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     */
+    public function next()
+    {
+        ++$this->position;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Return the key of the current element
+     * @link http://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     */
+    public function key()
+    {
+        return $this->position;
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Checks if current position is valid
+     * @link http://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     */
+    public function valid()
+    {
+        return isset($this->records[$this->position]);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Rewind the Iterator to the first element
+     * @link http://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     */
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+
 }
